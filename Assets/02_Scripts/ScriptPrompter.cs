e503c70c3dddac2dccd02c9d332954c3230f70ec
boracles <100pour100@naver.com>
2023-11-30 18:56:16 +0900

    Script on and off

--- Assets/02_Scripts/ScriptPrompter.cs
+++ Assets/02_Scripts/ScriptPrompter.cs
@@ -10,11 +10,73 @@ public class ScriptPrompter : MonoBehaviour
     public TextMeshProUGUI scriptText;
     public string scriptFileName;       // 대본 텍스트 파일 이름
     public float scrollSpeed = 2.0f;    // 텍스트 스크롤 속도 
-    
 
-    public void StartingScript()
+    private bool isScrolling = false;
+    private Coroutine scrollingCoroutine;
+    private bool isScriptLoaded = false;    // 대본이 로드되었는지 여부
+
+
+    public void ToggleScript()
     {
-        LoadScriptFromFile(scriptFileName);
+        if (!isScriptLoaded)
+        {
+            LoadScriptFromFile(scriptFileName);
+            isScriptLoaded = true;
+            Debug.Log("대본 로드됨");
+        }
+        else
+        {
+            ToggleScrolling();
+        }
+    }
+    
+    private void ToggleScrolling()
+    {
+        if (isScrolling)
+        {
+            // 스크롤 중지
+            StopScrolling();
+        }
+        else
+        {
+            // 스크롤 시작
+            StartScrolling();
+        }
+    }
+
+    public void CompleteScript()
+    {
+        StopScrolling();
+        isScriptLoaded = false;
+        scriptText.text = ""; // 텍스트 내용 비우기
+        Debug.Log("대본 완료 및 리셋");
+    }
+
+    private void StartScrolling()
+    {
+        if (!isScrolling)
+        {
+            if (scrollingCoroutine != null)
+            {
+                StopCoroutine(scrollingCoroutine);
+                Debug.Log("스크롤 중지");
+            }
+            
+            scrollingCoroutine = StartCoroutine(ScrollText());
+            isScrolling = true;
+            Debug.Log("스크롤 시작");
+        }
+    }
+
+    private void StopScrolling()
+    {
+        if (scrollingCoroutine != null)
+        {
+            StopCoroutine(scrollingCoroutine);
+            scrollingCoroutine = null;
+            Debug.Log("스크롤 중지");
+        }
+        isScrolling = false;
     }
 
     void LoadScriptFromFile(string fileName)
@@ -25,7 +87,7 @@ public class ScriptPrompter : MonoBehaviour
         {
             string scriptContent = File.ReadAllText(filePath);
             scriptText.text = scriptContent;
-            StartCoroutine(ScrollText());
+            StartScrolling();
         }
         else
         {
@@ -36,7 +98,7 @@ public class ScriptPrompter : MonoBehaviour
     IEnumerator ScrollText()
     {
         RectTransform textRectTransform = scriptText.GetComponent<RectTransform>();
-        float scrollDistance = textRectTransform.rect.height*2.0f; // 스크롤할 거리 설정
+        float scrollDistance = textRectTransform.rect.height*10.0f; // 스크롤할 거리 설정
 
         Vector2 startPosition = textRectTransform.anchoredPosition;
         Vector2 endPosition = new Vector2(startPosition.x, startPosition.y + scrollDistance);
@@ -46,5 +108,8 @@ public class ScriptPrompter : MonoBehaviour
             textRectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
             yield return null;
         }
+        
+        // 스크롤이 끝나면 스크롤 상태 업데이트
+        isScrolling = false;
     }
 }

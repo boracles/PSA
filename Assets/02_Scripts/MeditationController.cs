95092983e7d82bed9bc2c61dc5b8722c4c9db2f5
boracles <100pour100@naver.com>
2023-12-05 03:06:35 +0900

    명상 써클 변경

--- Assets/02_Scripts/MeditationController.cs
+++ Assets/02_Scripts/MeditationController.cs
@@ -6,17 +6,15 @@ using UnityEngine;
 public class MeditationController : MonoBehaviour
 {
     public GameObject meditationCircle;
-    private Material circleMaterial;
-    
     public AudioSource meditationVoice;
     public float rotationSpeed = 10.0f;
 
     private bool isRotating = false;
-
-
     private float currentRotation = 0.0f;
-    private float targetOpacity = 0.7f;
-    private bool increasingOpacity = true;
+    private Material childMaterial;
+    private ParticleSystem particleSystem;
+    private ParticleSystem.ShapeModule shapeModule;
+
     private void OnEnable()
     {
         meditationCircle.transform.parent.gameObject.SetActive(false);
@@ -24,13 +22,14 @@ public class MeditationController : MonoBehaviour
 
     void Start()
     {
-        circleMaterial = meditationCircle.GetComponent<MeshRenderer>().material;
+        childMaterial = meditationCircle.transform.GetChild(0).GetComponent<ParticleSystemRenderer>().material;
+        particleSystem = meditationCircle.transform.GetChild(1).GetComponent<ParticleSystem>();
+        shapeModule = particleSystem.shape;
     }
-
+    
     public void PlayMeditationVoice()
     {
         meditationVoice.Play();
-
         isRotating = true;
     }
 
@@ -38,31 +37,63 @@ public class MeditationController : MonoBehaviour
     {
         if (isRotating)
         {
-            // 부모 객체의 X축을 중심으로 회전
-            float rotationAmount = rotationSpeed * Time.deltaTime;
-            meditationCircle.transform.parent.Rotate(Vector3.right, rotationAmount);
-            currentRotation += rotationAmount;
-
-            // 한 바퀴 회전할 때마다 Opacity 변경
-            if (currentRotation >= 360.0f)
-            {
-                currentRotation = 0.0f;
-                increasingOpacity = !increasingOpacity; // Opacity 변경 방향 전환
-            }
-
-            // Material의 Opacity 조절
-            float opacityChangeSpeed = 0.6f * Time.deltaTime; // Opacity 변경 속도
-            float currentOpacity = circleMaterial.GetFloat("_Alpha");
-            if (increasingOpacity)
-            {
-                currentOpacity = Mathf.Min(currentOpacity + opacityChangeSpeed, targetOpacity);
-            }
-            else
-            {
-                currentOpacity = Mathf.Max(currentOpacity - opacityChangeSpeed, 0.1f);
-            }
-
-            circleMaterial.SetFloat("_Alpha", currentOpacity);
+            RotateMeditationCircle();
+            UpdateFresnelPower();
+            UpdateParticleSystemRadius();
         }
+    }
+
+    private void RotateMeditationCircle()
+    {
+        // 부모 객체의 X축을 중심으로 회전
+        float rotationAmount = rotationSpeed * Time.deltaTime;
+        meditationCircle.transform.parent.Rotate(Vector3.right, rotationAmount);
+        currentRotation = (currentRotation + rotationAmount) % 360.0f;
+    }
+
+    private void UpdateFresnelPower()
+    {
+        float fresnelPower;
+        if (currentRotation <= 180)
+        {
+            fresnelPower = 8 * (1 - currentRotation / 180.0f);
+        }
+        else
+        {
+            fresnelPower = 8 * ((currentRotation - 180) / 180.0f);
+        }
+
+        if (childMaterial != null)
+        {
+            childMaterial.SetFloat("_FresnelPower", fresnelPower);
+        }
+    }
+
+    private void UpdateParticleSystemRadius()
+    {
+        // 오디오 볼륨 레벨 계산
+        float[] spectrum = new float[256];
+        meditationVoice.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        float volumeLevel = GetRMS(spectrum);
+
+        // 오디오 볼륨에 따라 ParticleSystem의 Radius 값 조절
+        float radius = MapVolumeToRadius(volumeLevel);
+        shapeModule.radius = radius;
+    }
+
+    float GetRMS(float[] spectrum)
+    {
+        float rms = 0f;
+        for (int i = 0; i < spectrum.Length; i++)
+        {
+            rms += spectrum[i] * spectrum[i];
+        }
+        rms = Mathf.Sqrt(rms / spectrum.Length);
+        return rms;
+    }
+
+    float MapVolumeToRadius(float volume)
+    {
+        return Mathf.Clamp(volume * 50, 0, 3); // 오디오 음량에 따른 radius 값의 범위 조절
     }
 }

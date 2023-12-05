4c534ad45d86b7396a6d04f54e24414eb0a6c617
boracles <100pour100@naver.com>
2023-12-05 23:45:44 +0900

    폰트 새로 구움

--- Assets/02_Scripts/Timer.cs
+++ Assets/02_Scripts/Timer.cs
@@ -9,6 +9,7 @@ public class Timer : MonoBehaviour
     private float timeRemaining;    // 남은 시간
     private bool isTimerRunning = false; // 타이머가 실행 중인지를 나타내는 플래그
     public float initialTime = 180; // 초기 시간 (3분)
+    private bool isFlashing = false;
 
     void Start()
     {
@@ -20,12 +21,15 @@ public class Timer : MonoBehaviour
     {
         isTimerRunning = true;
         timeRemaining = initialTime;
+        isFlashing = false; // 깜빡임 초기화
+        timerText.color = Color.white; // 글자색 초기화
     }
 
     public void ResetTimer()
     {
         isTimerRunning = false; // 타이머를 정지합니다.
         timeRemaining = initialTime; // 시간을 초기 값으로 설정합니다.
+        timerText.color = Color.white; // 글자색 초기화
         DisplayTime(timeRemaining); // 타이머 표시를 업데이트합니다.
     }
     
@@ -37,6 +41,12 @@ public class Timer : MonoBehaviour
             {
                 timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
+                if (timeRemaining <= 10.0f && !isFlashing)
+                {
+                    // 10초 남았을 때 깜빡임 시작
+                    isFlashing = true;
+                    StartCoroutine(FlashTimerText());
+                }
             }
             else
             {
@@ -45,7 +55,17 @@ public class Timer : MonoBehaviour
             }
         }
     }
+    IEnumerator FlashTimerText()
+    {
+        timerText.color = Color.red; // 글자색을 빨간색으로 변경
 
+        while (isFlashing)
+        {
+            timerText.enabled = !timerText.enabled; // 텍스트 활성화/비활성화 전환
+            yield return new WaitForSeconds(0.5f); // 0.5초마다 전환
+        }
+    }
+    
     void DisplayTime(float timeToDisplay)
     {
         timeToDisplay += 1;
